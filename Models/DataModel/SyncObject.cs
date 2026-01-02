1a6d2a6aed8d1881f3ae595f708bcc040761ce20
Frooxius <tomas.mariancik@gmail.com>
2026-01-01 22:16:24 -0800

    feat: Add sync object support

---
+++ Models/DataModel/SyncObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ResoniteLink
+{
+    public class SyncObject : Member
+    {
+        /// <summary>
+        /// Members (fields, references, lists...) of this sync object and their data
+        /// </summary>
+        [JsonPropertyName("members")]
+        public Dictionary<string, Member> Members { get; set; }
+    }
+
+    [JsonDerivedType(typeof(Reference), "syncObject")]
+    public partial class Member { }
+}
